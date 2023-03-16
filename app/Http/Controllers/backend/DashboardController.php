5c68fe22ee9323b99f9b0e8a5df53fdfee7a6556
JI-Ujjal <jiujjal85@gmail.com>
2023-03-16 23:02:20 +0600

    mastering

---
+++ app/Http/Controllers/backend/DashboardController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function master(){
+        return view('backend.master');
+    }
+
+    public function dashboard(){
+        return view('backend.pages.dashboard');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\backend\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [DashboardController::class, 'dashboard'])->name('dashboard');
+
+Route::get('/master', [DashboardController::class, 'master'])->name('master');
